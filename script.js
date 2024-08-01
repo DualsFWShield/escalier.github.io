8d2677ac8d366eb9b62546ef451808c0aea4aa49
Noah Bruijninckx <66059559+DualsFWShield@users.noreply.github.com>
2024-08-01 05:25:26 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Ajoute un événement au formulaire pour valider les entrées
-document.querySelector('form').addEventListener('submit', function(event) {
-    // Vérifie si les champs sont remplis
-    if (document.querySelector('#player1_name').value === '' || document.querySelector('#player2_name').value === '' || document.querySelector('#plis_pronostiques').value === '') {
-        alert('Veuillez remplir tous les champs');
-        event.preventDefault();
-    }
-});
